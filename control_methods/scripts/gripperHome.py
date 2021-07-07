72345f11ae96f7787fc93f120d57360d5ca971f9
ROB <info@rob.uni-luebeck.de>
2021-07-07 14:58:36 +0200

    Gripper is connected with sensor glove
    Signed-off-by: ROB <info@rob.uni-luebeck.de>

--- control_methods/scripts/gripperHome.py
+++ control_methods/scripts/gripperHome.py
@@ -4,63 +4,68 @@ import rospy
 import actionlib
 from franka_gripper.msg import MoveGoal, MoveAction, GraspAction, StopAction, GraspGoal, StopGoal
 import curses
+from std_msgs.msg import Float32
 
-if __name__ == '__main__':
-    rospy.init_node('Franka_gripper_move_action')
-    client = actionlib.SimpleActionClient('/franka_gripper/move', MoveAction)
-    client.wait_for_server()
-    goal = MoveGoal(width = 0.1, speed = 0.04)
-    client.send_goal(goal)
-    client.wait_for_result(rospy.Duration.from_sec(5.0))
+class joint:
+    def __init__(self):
+        rospy.Subscriber("/force_joints/hand/index_finger_joint_1", Float32, self.callback)
+        self.data = Float32()
+        self.flag = False
+        
 
-    max_open = 0.1
-    min_open = 0
-    set_width = 0.1
-    #step_size = 0.01
-    grasp = 0.07
-    release = 0.09 
- 
-    # get the curses screen window
-    screen = curses.initscr()
-    
-    # turn off input echoing
-    curses.noecho()
-    
-    # respond to keys immediately (don't wait for enter)
-    curses.cbreak()
-    
-    # map arrow keys to special values
-    screen.keypad(True)
+    def callback(self, msg):
+        self.grasping(msg)
 
-    loop = True
-    
+    def grasping(self,data):
+        set_width = 0.1
+        max_open = 0.1
+        min_open = 0
+        grasp = 0.07
+        release = 0.09 
 
-    while loop:
-        char = screen.getch()
-        if char == ord('q'):
-            break
-        elif char == curses.KEY_RIGHT:
-            # print doesn't work with curses, use addstr instead
-            screen.addstr(0, 0, 'open gripper (right)')
+        print(data)
+        if float(data.data) < 1.0:
+            print('open')
             if (set_width <= max_open):
+                self.flag = False
                 client = actionlib.SimpleActionClient('/franka_gripper/move', MoveAction)
                 client.wait_for_server()
                 #set_width += step_size
                 set_width = release
                 goal = MoveGoal(width = set_width, speed = 0.04)
                 client.send_goal(goal) 
-        elif char == curses.KEY_LEFT:
-            screen.addstr(0, 0, 'close gripper (left)')   
+            bend = index.data
+        elif (float(data.data) > 1.2) & (self.flag == False):
+            print('grasp')
+            self.flag = True
             if (set_width >= min_open):
                 client = actionlib.SimpleActionClient('/franka_gripper/grasp', GraspAction)
                 client.wait_for_server()
                 #set_width -= step_size
                 set_width = grasp
-                action = GraspGoal(width = set_width, speed = 0.04, force = 1) 
-                client.send_goal(action) 
-        elif char == curses.KEY_UP:
-            curses.nocbreak(); screen.keypad(0); curses.echo()
-            curses.endwin()  
-            loop = False   
+                action = GraspGoal(width = 0.05, speed = 0.04, force = 1) 
+                client.send_goal(action)
+        
+
+def init_gripper():
+    rospy.init_node('Franka_gripper_move_action')
+    client = actionlib.SimpleActionClient('/franka_gripper/move', MoveAction)
+    client.wait_for_server()
+    goal = MoveGoal(width = 0.1, speed = 0.04)
+    client.send_goal(goal)
+    client.wait_for_result(rospy.Duration.from_sec(5.0))   
+       
+
+
+if __name__ == '__main__':
+
+    init_gripper()
+
+    loop = True
+    
+    index = joint()
+    rospy.spin()
+    
+           
 
         
